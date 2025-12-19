6913bccd3d13956d0ea17b892fe6ea564212f322
JunDevl <juninhoplay.pedone@gmail.com>
2025-12-18 21:27:41 -0300

    Implemented hover logic for ship placement in the UI

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,113 @@
 import "./style.css";
 
+const MAXIMUM_SHIP_COORDINATE = 10;
+
 const gameboardGrid = document.querySelector("ul.grid") as HTMLDivElement;
+const shipSelect = document.querySelector("select#ships") as HTMLSelectElement;
+
+let highlightedCells: HTMLLIElement[] = [];
+
+let currentShipLength = 0;
+
+type Degree = number;
+let rotation: Degree = 270;
 
 generateGrid();
 
-gameboardGrid.addEventListener("mouseover", (e) => {
-  const element = e.target as HTMLElement;
-
-  if (element.className === "cell") {
-    element.style.backgroundColor = "yellow";
-  }
+shipSelect.addEventListener("input", (e) => {
+  const target = e.target as HTMLSelectElement
+  currentShipLength = Number(target.value);
 })
 
-gameboardGrid.addEventListener("mouseout", (e) => {
-  const element = e.target as HTMLElement;
+gameboardGrid.addEventListener("mouseover", (e) => {
+  const target = e.target as HTMLLIElement;
 
-  if (element.className === "cell") {
-    element.style.backgroundColor = "rgb(195, 255, 255)"
+  if (target.className !== "cell" || currentShipLength === 0) return;
+
+  highlightedCells.forEach(cell => cell.style.backgroundColor = "rgb(195, 255, 255)");
+
+  changeHighlightOrientation(target);
+
+  highlightedCells.forEach(cell => cell.style.backgroundColor = "yellow");
+})
+ 
+gameboardGrid.addEventListener("mouseleave", (e) => {
+  highlightedCells.forEach(cell => cell.style.backgroundColor = "rgb(195, 255, 255)");
+  highlightedCells = [];
+})
+
+gameboardGrid.addEventListener("wheel", (e) => {
+  e.preventDefault();
+
+  const target = e.target as HTMLLIElement;
+
+  if (e.deltaY < 0) {
+    rotation = rotation + 90 === 360 ? 0 : rotation + 90;
+  } 
+  else {
+    rotation = rotation - 90 < 0 ? 270 : rotation - 90;
   }
+
+  highlightedCells.forEach(cell => cell.style.backgroundColor = "rgb(195, 255, 255)");
+
+  changeHighlightOrientation(target);
+
+  highlightedCells.forEach(cell => cell.style.backgroundColor = "yellow");
 })
 
 function generateGrid() {
-  for (let row = 1; row <= 10; row++) {
-    for (let column = 1; column <= 10; column++) {
+  for (let row = 1; row <= MAXIMUM_SHIP_COORDINATE; row++) {
+    for (let column = 1; column <= MAXIMUM_SHIP_COORDINATE; column++) {
       const cell = document.createElement("li");
       cell.id = `${column}:${row}`;
+      cell.setAttribute("x", column.toString());
+      cell.setAttribute("y", row.toString());
       cell.className = "cell";
 
       gameboardGrid.appendChild(cell);
     }
+  }
+}
+
+function iterateShipHighlight(indexMutation: "increment" | "decrement", orientation: "horizontal" | "vertical", originElement: HTMLLIElement) {
+  const [originX, originY] = [Number(originElement.getAttribute("x")), Number(originElement.getAttribute("y"))] as [number, number];
+
+  if (indexMutation === "increment") {
+    if ((orientation === "horizontal" && originX + currentShipLength > MAXIMUM_SHIP_COORDINATE + 1) ||
+        (orientation === "vertical" && originY + currentShipLength > MAXIMUM_SHIP_COORDINATE + 1))
+      return;
+  } 
+
+  if (indexMutation === "decrement") {
+    if ((orientation === "horizontal" && originX - currentShipLength < 0) ||
+        (orientation === "vertical" && originY - currentShipLength < 0))
+      return;
+  }
+
+  highlightedCells = [];
+
+  for (let i = 0; indexMutation === "increment" ? i < currentShipLength : i > -currentShipLength; indexMutation === "increment" ? i++ : i--) {
+    const selector = orientation === "horizontal" ? `li[id="${originX + i}:${originY}"]` : `li[id="${originX}:${originY + i}"]`
+    const nextElement: HTMLLIElement | null = document.querySelector(selector);
+
+    if (nextElement) highlightedCells.push(nextElement);
+  }
+}
+
+function changeHighlightOrientation(originElement: HTMLLIElement) {
+  switch (rotation) {
+    case 0:
+      console.log('here')
+      iterateShipHighlight("increment", "horizontal", originElement);
+      break;
+    case 90:
+      iterateShipHighlight("decrement", "vertical", originElement);
+      break;
+    case 180:
+      iterateShipHighlight("decrement", "horizontal", originElement);
+      break;
+    case 270:
+      iterateShipHighlight("increment", "vertical", originElement);
+      break;
   }
 }
